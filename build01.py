d1f40cc5388edc43acfba8a0a5d0cb42fdb17fda
Jeremy Ellis <keyfreemusic@gmail.com>
2020-10-06 13:03:37 -0700

    Create build01.py

---
+++ build01.py
@@ -0,0 +1,9 @@
+class CircuitLayerBuilder():
+    def __init__(self, data_qubits, readout):
+        self.data_qubits = data_qubits
+        self.readout = readout
+    
+    def add_layer(self, circuit, gate, prefix):
+        for i, qubit in enumerate(self.data_qubits):
+            symbol = sympy.Symbol(prefix + '-' + str(i))
+            circuit.append(gate(qubit, self.readout)**symbol)
