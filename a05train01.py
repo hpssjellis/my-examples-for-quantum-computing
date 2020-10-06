0683a245deff6b7b3e871cff1637f974a3a13025
Jeremy Ellis <keyfreemusic@gmail.com>
2020-10-06 14:45:14 -0700

    Create a05train01.py

---
+++ a05train01.py
@@ -0,0 +1,23 @@
+EPOCHS = 3
+BATCH_SIZE = 32
+
+NUM_EXAMPLES = len(x_train_tfcirc)
+
+x_train_tfcirc_sub = x_train_tfcirc[:NUM_EXAMPLES]
+y_train_hinge_sub = y_train_hinge[:NUM_EXAMPLES]
+
+qnn_history = model.fit(
+      x_train_tfcirc_sub, y_train_hinge_sub,
+      batch_size=32,
+      epochs=EPOCHS,
+      verbose=1,
+      validation_data=(x_test_tfcirc, y_test_hinge))
+
+qnn_results = model.evaluate(x_test_tfcirc, y_test)
+
+
+
+
+
+
+
