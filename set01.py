b805d03033c46e9ca1168fd40dae3d5f9cfb1692
Jeremy Ellis <keyfreemusic@gmail.com>
2020-10-06 12:58:08 -0700

    Create set01.py

---
+++ set01.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+import tensorflow_quantum as tfq
+
+import cirq
+import sympy
+import numpy as np
+import seaborn as sns
+import collections
+
+# visualization tools
+%matplotlib inline
+import matplotlib.pyplot as plt
+from cirq.contrib.svg import SVGCircuit
