640b32e34346502560c37786351c5e13346e4e54
Jeremy Ellis <keyfreemusic@gmail.com>
2020-10-09 16:41:52 -0700

    Update a06draft.py

--- a06draft.py
+++ a06draft.py
@@ -3,6 +3,10 @@ from keras.layers.core import Dense, Dropout, Activation
 from keras.optimizers import SGD
 import numpy as np 
 
+
+
+print("Classical Keras xOr model saving using Tensorflow Python")
+
 X = np.array([[0,0],[0,1],[1,0],[1,1]])
 y = np.array([[0],[1],[1],[0]])
 
