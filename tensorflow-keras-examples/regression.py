181d991e0073e5de53172c394c8e76b24b6fd02d
Jeremy Ellis <keyfreemusic@gmail.com>
2020-10-10 08:39:46 -0700

    Add files via upload

---
+++ tensorflow-keras-examples/regression.py
@@ -0,0 +1,262 @@
+#@title Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#@title MIT License
+#
+# Copyright (c) 2017 François Chollet
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
+# Use seaborn for pairplot
+!pip install -q seaborn
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+
+# Make numpy printouts easier to read.
+np.set_printoptions(precision=3, suppress=True)
+
+import tensorflow as tf
+
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.layers.experimental import preprocessing
+
+print(tf.__version__)
+
+url = 'http://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.data'
+column_names = ['MPG', 'Cylinders', 'Displacement', 'Horsepower', 'Weight',
+                'Acceleration', 'Model Year', 'Origin']
+
+raw_dataset = pd.read_csv(url, names=column_names,
+                          na_values='?', comment='\t',
+                          sep=' ', skipinitialspace=True)
+
+dataset = raw_dataset.copy()
+dataset.tail()
+
+dataset.isna().sum()
+
+dataset = dataset.dropna()
+
+dataset['Origin'] = dataset['Origin'].map({1: 'USA', 2: 'Europe', 3: 'Japan'})
+
+dataset = pd.get_dummies(dataset, prefix='', prefix_sep='')
+dataset.tail()
+
+train_dataset = dataset.sample(frac=0.8, random_state=0)
+test_dataset = dataset.drop(train_dataset.index)
+
+sns.pairplot(train_dataset[['MPG', 'Cylinders', 'Displacement', 'Weight']], diag_kind='kde')
+
+train_dataset.describe().transpose()
+
+train_features = train_dataset.copy()
+test_features = test_dataset.copy()
+
+train_labels = train_features.pop('MPG')
+test_labels = test_features.pop('MPG')
+
+train_dataset.describe().transpose()[['mean', 'std']]
+
+normalizer = preprocessing.Normalization()
+
+normalizer.adapt(np.array(train_features))
+
+print(normalizer.mean.numpy())
+
+first = np.array(train_features[:1])
+
+with np.printoptions(precision=2, suppress=True):
+  print('First example:', first)
+  print()
+  print('Normalized:', normalizer(first).numpy())
+
+horsepower = np.array(train_features['Horsepower'])
+
+horsepower_normalizer = preprocessing.Normalization(input_shape=[1,])
+horsepower_normalizer.adapt(horsepower)
+
+horsepower_model = tf.keras.Sequential([
+    horsepower_normalizer,
+    layers.Dense(units=1)
+])
+
+horsepower_model.summary()
+
+horsepower_model.predict(horsepower[:10])
+
+horsepower_model.compile(
+    optimizer=tf.optimizers.Adam(learning_rate=0.1),
+    loss='mean_absolute_error')
+
+%%time
+history = horsepower_model.fit(
+    train_features['Horsepower'], train_labels,
+    epochs=100,
+    # suppress logging
+    verbose=0,
+    # Calculate validation results on 20% of the training data
+    validation_split = 0.2)
+
+hist = pd.DataFrame(history.history)
+hist['epoch'] = history.epoch
+hist.tail()
+
+def plot_loss(history):
+  plt.plot(history.history['loss'], label='loss')
+  plt.plot(history.history['val_loss'], label='val_loss')
+  plt.ylim([0, 10])
+  plt.xlabel('Epoch')
+  plt.ylabel('Error [MPG]')
+  plt.legend()
+  plt.grid(True)
+
+plot_loss(history)
+
+test_results = {}
+
+test_results['horsepower_model'] = horsepower_model.evaluate(
+    test_features['Horsepower'],
+    test_labels, verbose=0)
+
+x = tf.linspace(0.0, 250, 251)
+y = horsepower_model.predict(x)
+
+def plot_horsepower(x, y):
+  plt.scatter(train_features['Horsepower'], train_labels, label='Data')
+  plt.plot(x, y, color='k', label='Predictions')
+  plt.xlabel('Horsepower')
+  plt.ylabel('MPG')
+  plt.legend()
+
+plot_horsepower(x,y)
+
+linear_model = tf.keras.Sequential([
+    normalizer,
+    layers.Dense(units=1)
+])
+
+linear_model.predict(train_features[:10])
+
+linear_model.layers[1].kernel
+
+linear_model.compile(
+    optimizer=tf.optimizers.Adam(learning_rate=0.1),
+    loss='mean_absolute_error')
+
+%%time
+history = linear_model.fit(
+    train_features, train_labels, 
+    epochs=100,
+    # suppress logging
+    verbose=0,
+    # Calculate validation results on 20% of the training data
+    validation_split = 0.2)
+
+plot_loss(history)
+
+test_results['linear_model'] = linear_model.evaluate(
+    test_features, test_labels, verbose=0)
+
+def build_and_compile_model(norm):
+  model = keras.Sequential([
+      norm,
+      layers.Dense(64, activation='relu'),
+      layers.Dense(64, activation='relu'),
+      layers.Dense(1)
+  ])
+
+  model.compile(loss='mean_absolute_error',
+                optimizer=tf.keras.optimizers.Adam(0.001))
+  return model
+
+dnn_horsepower_model = build_and_compile_model(horsepower_normalizer)
+
+dnn_horsepower_model.summary()
+
+%%time
+history = dnn_horsepower_model.fit(
+    train_features['Horsepower'], train_labels,
+    validation_split=0.2,
+    verbose=0, epochs=100)
+
+plot_loss(history)
+
+x = tf.linspace(0.0, 250, 251)
+y = dnn_horsepower_model.predict(x)
+
+plot_horsepower(x, y)
+
+test_results['dnn_horsepower_model'] = dnn_horsepower_model.evaluate(
+    test_features['Horsepower'], test_labels,
+    verbose=0)
+
+dnn_model = build_and_compile_model(normalizer)
+dnn_model.summary()
+
+%%time
+history = dnn_model.fit(
+    train_features, train_labels,
+    validation_split=0.2,
+    verbose=0, epochs=100)
+
+plot_loss(history)
+
+test_results['dnn_model'] = dnn_model.evaluate(test_features, test_labels, verbose=0)
+
+pd.DataFrame(test_results, index=['Mean absolute error [MPG]']).T
+
+test_predictions = dnn_model.predict(test_features).flatten()
+
+a = plt.axes(aspect='equal')
+plt.scatter(test_labels, test_predictions)
+plt.xlabel('True Values [MPG]')
+plt.ylabel('Predictions [MPG]')
+lims = [0, 50]
+plt.xlim(lims)
+plt.ylim(lims)
+_ = plt.plot(lims, lims)
+
+
+error = test_predictions - test_labels
+plt.hist(error, bins=25)
+plt.xlabel('Prediction Error [MPG]')
+_ = plt.ylabel('Count')
+
+dnn_model.save('dnn_model')
+
+reloaded = tf.keras.models.load_model('dnn_model')
+
+test_results['reloaded'] = reloaded.evaluate(
+    test_features, test_labels, verbose=0)
+
+pd.DataFrame(test_results, index=['Mean absolute error [MPG]']).T
