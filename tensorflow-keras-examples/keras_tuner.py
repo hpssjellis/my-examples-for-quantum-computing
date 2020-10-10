39b8cac5fd89df4096e6c33e2c3943990cf86ce5
Jeremy Ellis <keyfreemusic@gmail.com>
2020-10-10 08:44:09 -0700

    Update keras_tuner.py

--- tensorflow-keras-examples/keras_tuner.py
+++ tensorflow-keras-examples/keras_tuner.py
@@ -13,9 +13,9 @@
 import tensorflow as tf
 from tensorflow import keras
 
-import IPython
+#import IPython
 
-!pip install -U keras-tuner
+pip3 install -U keras-tuner
 import kerastuner as kt
 
 (img_train, label_train), (img_test, label_test) = keras.datasets.fashion_mnist.load_data()
