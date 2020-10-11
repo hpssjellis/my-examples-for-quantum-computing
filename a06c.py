1e4ebf942eaf40f0814d1e5a1e16489c6d79cee5
Jeremy Ellis <keyfreemusic@gmail.com>
2020-10-11 14:14:57 -0700

    Create a06c.py

---
+++ a06c.py
@@ -0,0 +1,53 @@
+#@title Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import tensorflow as tf
+import numpy as np
+
+x_train = np.array([[0,0],[0,1],[1,0],[1,1]]).astype("float32")
+y_train = np.array([[0],[1],[1],[0]]).astype("float32")
+
+
+
+
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Dense(10, input_shape=(2,)),
+  tf.keras.layers.Dense(2)
+])
+
+predictions = model(x_train[:1]).numpy()
+predictions
+
+tf.nn.softmax(predictions).numpy()
+
+loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+loss_fn(y_train[:1], predictions).numpy()
+
+model.compile(optimizer='adam',
+              loss=loss_fn,
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=500)
+
+#model.evaluate(x_test,  y_test, verbose=2)
+
+probability_model = tf.keras.Sequential([
+  model,
+  tf.keras.layers.Softmax()
+])
+
+probability_model(x_train[:5])
+
+myResult = model.predict(x_train)
+
+print(myResult[0])
