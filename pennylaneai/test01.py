3740a7d95db43e12911e92a06cd1f45649463c99
Jeremy Ellis <keyfreemusic@gmail.com>
2020-11-10 19:43:12 -0800

    Create test01.py

---
+++ pennylaneai/test01.py
@@ -0,0 +1,15 @@
+import pennylane as qml
+
+dev = qml.device('default.qubit', wires=2, analytic=True)
+
+@qml.qnode(dev)
+def circuit(x=None, y=None):
+    qml.BasisState(np.array([x,y]), wires=[0,1])
+    qml.CNOT(wires=[0,1])
+    return qml.probs(wires=[1])
+
+# Get the probability of the first wire being in state 1
+print(circuit(x=0,y=0)[1])
+print(circuit(x=0,y=1)[1])
+print(circuit(x=1,y=0)[1])
+print(circuit(x=1,y=1)[1])
