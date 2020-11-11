93e3264122788701fc073e522ba5b4633ec9116e
Jeremy Ellis <keyfreemusic@gmail.com>
2020-11-10 20:04:20 -0800

    Create test04.py

---
+++ pennylaneai/test04.py
@@ -0,0 +1,16 @@
+import pennylane as qml
+import numpy as np
+
+
+
+n_qubits = 2
+dev = qml.device("default.qubit", wires=n_qubits)
+
+@qml.qnode(dev)
+def qnode(inputs, weights_0, weight_1):
+    qml.RX(inputs[0], wires=0)
+    qml.RX(inputs[1], wires=1)
+    qml.Rot(*weights_0, wires=0)
+    qml.RY(weight_1, wires=1)
+    qml.CNOT(wires=[0, 1])
+    return qml.expval(qml.PauliZ(0)), qml.expval(qml.PauliZ(1))
