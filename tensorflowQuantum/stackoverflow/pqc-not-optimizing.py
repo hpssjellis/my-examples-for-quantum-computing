0f0ff62cce212f6adab02475fe715b8702000159
Jeremy Ellis <keyfreemusic@gmail.com>
2020-11-16 23:06:27 -0800

    Update pqc-not-optimizing.py

--- tensorflowQuantum/stackoverflow/pqc-not-optimizing.py
+++ tensorflowQuantum/stackoverflow/pqc-not-optimizing.py
@@ -60,7 +60,7 @@ def hinge_accuracy(y_true, y_pred):
     y_true = tf.squeeze(y_true) > 0.0
     y_pred = tf.squeeze(y_pred) > 0.0
     result = tf.cast(y_true == y_pred, tf.float32)
-return tf.reduce_mean(result)
+    return tf.reduce_mean(result)
 
 # compile the model with Hinge loss and Adam, as done in the example. Have tried with various learning_rates
 model.compile(
