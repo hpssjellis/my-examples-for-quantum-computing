24f54064fd14afd9df114969deedb45f5d4dbabb
Jeremy Ellis <keyfreemusic@gmail.com>
2020-11-24 15:12:13 -0800

    Update a02-set-antlr4-python3-runtime-to-4.7.2.sh

--- pennylaneai/qml-demos/a02-set-antlr4-python3-runtime-to-4.7.2.sh
+++ pennylaneai/qml-demos/a02-set-antlr4-python3-runtime-to-4.7.2.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-pip3 install antlr4-python3-runtime==4.7.2
+pip3 install antlr4-python3-runtime==4.7.2  pyquil==2.19.0
+
+
+# pyquil now at 2.25 https://github.com/rigetti/pyquil/releases/tag/v2.25.0
+# pyquil==2.21  July 14th
+
+
+#   Apr 17, 2020    4.7.2
+#    latest 4.9  https://github.com/antlr/antlr4/releases
