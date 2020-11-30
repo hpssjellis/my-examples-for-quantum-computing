07ca108e3efa9b7605d34a4ab9e5048991ea30b1
Jeremy Ellis <keyfreemusic@gmail.com>
2020-11-29 21:04:06 -0800

    Update qnn-show-rocksetta.py

--- rocksetta/qnn-show-rocksetta.py
+++ rocksetta/qnn-show-rocksetta.py
@@ -1,5 +1,6 @@
-
-## from   https://pennylane.ai/qml/demos/tutorial_qnn_module_tf.html
+## Attempt to simplify Small Quantum Keras models
+## By Rocksetta
+## from  https://pennylane.ai/qml/demos/tutorial_qnn_module_tf.html
 ## from  https://github.com/hpssjellis/my-examples-for-quantum-computing/blob/main/pennylaneai/qml-demos/tf-tutorial_qnn_module_tf.py
 
 
@@ -17,7 +18,6 @@ print("y")
 print(y)
 
 y_hot = tf.keras.utils.to_categorical(y, num_classes=2)  # one-hot encoded labels
-
 
 print()
 print("y_hot")
@@ -43,11 +43,10 @@ def qnode(inputs, weights):
 n_layers = 6
 weight_shapes = {"weights": (n_layers, n_qubits)}
 
-qlayer = qml.qnn.KerasLayer(qnode, weight_shapes, output_dim=n_qubits)
-
-clayer_1 = tf.keras.layers.Dense(2)
-clayer_2 = tf.keras.layers.Dense(2, activation="softmax")
-model = tf.keras.models.Sequential([clayer_1, qlayer, clayer_2])
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Dense(2))
+model.add(qml.qnn.KerasLayer(qnode, weight_shapes, output_dim=n_qubits))
+model.add(tf.keras.layers.Dense(2, activation="softmax"))
 
 opt = tf.keras.optimizers.SGD(learning_rate=0.2)
 model.compile(opt, loss="mae", metrics=["accuracy"])
@@ -57,17 +56,16 @@ y_hot = y_hot.astype("float32")
 fitting = model.fit(X, y_hot, epochs=3, batch_size=5, validation_split=0.25, verbose=2)
 model.summary()
 
-
-
 qnn_results = model.evaluate(X, y_hot, verbose=2)
-
 
 print ("qnn_results")
 print (qnn_results)
-
 
 #tf.keras.models.save_model(model, './model11.h5', overwrite=True, include_optimizer=True, save_format=None, signatures=None, options=None)
 tf.keras.models.save_model(model, './model12.h5')
 
 #model.save_weights('./model_weights12')
+
+
+
 
