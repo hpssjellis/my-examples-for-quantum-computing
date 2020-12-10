5235c682b0d2de2eaab8d52b84e295c9f4abf4e5
Jeremy Ellis <keyfreemusic@gmail.com>
2020-12-10 13:40:29 -0800

    Create setup-IBM.py

---
+++ qiskit/setup-IBM.py
@@ -0,0 +1,10 @@
+#from qiskit import IBMQ
+#IBMQ.save_account('MY_API_TOKEN')
+#
+#login and find your IBM Quantum computing account token
+#https://quantum-computing.ibm.com/account
+#
+
+
+from qiskit import IBMQ
+IBMQ.save_account('MY_API_TOKEN')
